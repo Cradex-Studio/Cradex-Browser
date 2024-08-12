f195d071792ef912e517c007e931417024e6de38
Findof <joeargon21@gmail.com>
2024-08-12 12:37:52 -0400

    Added more functionality to the lexer (small)

--- src/Includes/DOMNode.h
+++ src/Includes/DOMNode.h
@@ -8,6 +8,7 @@ enum NodeType
   HTML,
   Body,
   Head,
+  Title,
   P,
   Text,
   H1,
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -62,6 +62,22 @@ std::vector<HTMLToken> tokenizeHTML(const std::string &src)
         });
         index += 7;
       }
+      else if (src.substr(index, 7) == "<title>")
+      {
+        tokens.push_back({
+          type : TitleOpen,
+          content : ""
+        });
+        index += 7;
+      }
+      else if (src.substr(index, 8) == "</title>")
+      {
+        tokens.push_back({
+          type : TitleClose,
+          content : ""
+        });
+        index += 8;
+      }
       else if (src.substr(index, 6) == "<head>")
       {
         tokens.push_back({
@@ -77,6 +93,102 @@ std::vector<HTMLToken> tokenizeHTML(const std::string &src)
           content : ""
         });
         index += 7;
+      }
+      else if (src.substr(index, 4) == "<h1>")
+      {
+        tokens.push_back({
+          type : H1Open,
+          content : ""
+        });
+        index += 4;
+      }
+      else if (src.substr(index, 5) == "</h1>")
+      {
+        tokens.push_back({
+          type : H1Close,
+          content : ""
+        });
+        index += 5;
+      }
+      else if (src.substr(index, 4) == "<h2>")
+      {
+        tokens.push_back({
+          type : H2Open,
+          content : ""
+        });
+        index += 4;
+      }
+      else if (src.substr(index, 5) == "</h2>")
+      {
+        tokens.push_back({
+          type : H2Close,
+          content : ""
+        });
+        index += 5;
+      }
+      else if (src.substr(index, 4) == "<h3>")
+      {
+        tokens.push_back({
+          type : H3Open,
+          content : ""
+        });
+        index += 4;
+      }
+      else if (src.substr(index, 5) == "</h3>")
+      {
+        tokens.push_back({
+          type : H3Close,
+          content : ""
+        });
+        index += 5;
+      }
+      else if (src.substr(index, 4) == "<h4>")
+      {
+        tokens.push_back({
+          type : H4Open,
+          content : ""
+        });
+        index += 4;
+      }
+      else if (src.substr(index, 5) == "</h4>")
+      {
+        tokens.push_back({
+          type : H4Close,
+          content : ""
+        });
+        index += 5;
+      }
+      else if (src.substr(index, 4) == "<h5>")
+      {
+        tokens.push_back({
+          type : H5Open,
+          content : ""
+        });
+        index += 4;
+      }
+      else if (src.substr(index, 5) == "</h5>")
+      {
+        tokens.push_back({
+          type : H5Close,
+          content : ""
+        });
+        index += 5;
+      }
+      else if (src.substr(index, 4) == "<h6>")
+      {
+        tokens.push_back({
+          type : H6Open,
+          content : ""
+        });
+        index += 4;
+      }
+      else if (src.substr(index, 5) == "</h6>")
+      {
+        tokens.push_back({
+          type : H6Close,
+          content : ""
+        });
+        index += 5;
       }
     }
     else
